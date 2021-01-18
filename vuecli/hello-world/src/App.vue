90057ace08996d37ea2de03ad8e8f0b2b0cccded
vidalmatheus <matheusvidaldemenezes@gmail.com>
2021-01-17 23:42:18 -0300

    Add vuecli template helloworld

---
+++ vuecli/hello-world/src/App.vue
@@ -0,0 +1,35 @@
+// begin component template
+<template>
+  <div id="app" style="border: solid;">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Vidal Vue.js App"/>
+  </div>
+</template>
+// end of component tempĺate
+
+// begin JS
+<script>
+// use any name you want for component importing (local register)
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+// ending JS
+
+// begin CSS
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+// ending CSS
---
+++ vuecli/hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <!-- dado(msg) desceu -->
+    <h1>Da raiz, o dado desceu como propriedade para a componente HelloWorld: '{{ msg }}' </h1>
+    <h2 class="my-comment" style="border: solid;">Da componente HelloWorld: '{{ dataMsg }}'</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataMsg: 'Vue ate que e dahora!',
+      v1: true,
+    };
+  },
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  methods: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.my-comment {
+  color: cadetblue;
+  font-weight: bold;
+}
+</style>
