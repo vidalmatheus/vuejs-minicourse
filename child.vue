7dead49be435a23b2d30ac8b382424e6957debbc
matheus.vidal <matheus.vidal@buser.com.br>
2021-01-24 01:27:43 -0300

    Add SFC-without-component try

---
+++ child.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="incrementCounter"> {{ counter }} </button>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        incrementCounter () {
+            thus.counter++
+            this.$emit('increment') // chama evento do v-on na componente pai
+        }
+    }
+}
+</script>
---
+++ parent.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <p>{{ total }}</p>
+    <child @increment="incrementTotal" />
+    <child @increment="incrementTotal" />
+</div>
+</template>
+
+<script>
+import child from './child'
+
+export default {
+  components: { child },
+  data () {
+      return {
+          total: 0
+      }
+  },
+  methods: {
+      incrementTotal () {
+          this.total++
+      }
+  }
+}
+</script>
